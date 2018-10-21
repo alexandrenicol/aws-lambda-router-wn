6b5aabc106a0a9ea2a4146fb4dc579f2453c6fb5
Dane Schneider <dane.schneider@gmail.com>
2018-10-21 13:51:51 -0700

    whitespace

--- index.d.ts
+++ index.d.ts
@@ -7,19 +7,12 @@ export interface Options {responseType: string, bodyType: string}
 export type MethodHandler = (path: string, response: Responder, options?: Options)=> void
 
 declare class AWSLambdaRouter {
-
   constructor()
-
   public serve: (event: APIGatewayEvent, callback: APIGatewayProxyCallback)=> void
-
   public get: MethodHandler
-
   public post: MethodHandler
-
   public delete: MethodHandler
-
   public useCors: (option: boolean)=> void
 }
-
 
 export default AWSLambdaRouter

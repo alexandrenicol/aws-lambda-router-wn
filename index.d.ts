adc6aa536386d1b3e9acbb98f921d6a66c02744a
Dane Schneider <dane.schneider@gmail.com>
2018-10-21 14:11:33 -0700

    fix handler type

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,9 @@ export type Responder = (err: Error, data: string | ProxyResult)=> void
 
 export interface Options {responseType: string, bodyType: string}
 
-export type MethodHandler = (path: string, response: Responder, options?: Options)=> void
+export type RouteHandler = (request: APIGatewayEvent, response: Responder)=> void
+
+export type MethodHandler = (path: string, handler: RouteHandler, options?: Options)=> void
 
 declare class AWSLambdaRouter {
   constructor()
